629b8fe2ea2b00e73a3e6d1b6ef1c9c2dd9a747e
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-06-05 10:03:26 -0400

    I did migrations

--- lib/blergers.rb
+++ lib/blergers.rb
@@ -9,7 +9,43 @@ module Blergers
   class Post < ActiveRecord::Base
     has_many :post_tags
     has_many :tags, through: :post_tags
+
+#Post.page(n) to return a page of blog results in reverse chronological order. I suggest pages of size 10. I.e.
+# Post.page(1) should return the last 10 posts I wrote, Post.page(2) should return the next 10 posts after that.
+
+  def self.page(n)
+    Post.select(:date).limit(10).order(date: :desc).offset(n)
+  #page_size = 10
+  #page_offset = (n -1)* page_size
+  #Post.order(date: :desc).offset(page_offset).limit(page_size)
   end
+
+# Tag.top_tags to print a ranking of the tags from most commonly used
+# to least commonly used, similar to last night's "scoreboard".
+  def self.top_tags
+    #Blergers::Tag.all.map {|x| [x.name, x.post.count]}.sort_by {|x| x[1]}.reverse (Doing this using ruby)
+
+    #Blergers::Tag.joins(:post_tags).
+    #group_by {|x| x.name}.
+    #map {|k, v| [k,v.length]}.
+    #sort_by {|x| x[1]}.
+    #reverse
+
+    #Tag.joins(:post_tags).group("tags.name").count.sort_by {|x| x[1]}.reverse
+
+    Tag.joins(:post_tags).group("tags.name").order("count_all DESC").count
+  end
+  #hard mode
+  def self.count_tagged_with(*tags_name)
+    Tag.joins(:post_tags).where(name: tags_name).goup(:post_id).count.count
+
+
+  end
+
+
+  end
+
+
 
   class Tag < ActiveRecord::Base
     has_many :post_tags
@@ -33,6 +69,8 @@ def add_post!(post)
   post_model = Blergers::Post.create(post)
   puts "New post! #{post_model}"
 end
+
+
 
 def run!
   blog_path = '/Users/brit/projects/improvedmeans'
